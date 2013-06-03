d781cca87e411d3c9c7d5fd589497481a9a3b27f
danielp1000 <leinad.ffafp@gmail.com>
2013-06-03 17:59:40 +0200

    The startup-script now runs cstart after loading

--- src/start.lua
+++ src/start.lua
@@ -1,3 +1,7 @@
 shell.run("Cauldron")
 term.setTextColor(colors.lightGray)
 print(Cauldron, " loaded")
+if fs.exists("cstart") and not fs.isDir("cstart") then
+    term.setTextColor(colors.white)
+    shell.run("cstart")
+end
