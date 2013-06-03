ae972099343ca4c2ac33e57ce5747a6c0a8de580
danielp1000 <leinad.ffafp@gmail.com>
2013-06-03 20:43:07 +0200

    Added parsing to cdoc (not finished)

--- src/CCore/CStringList.lua
+++ src/CCore/CStringList.lua
@@ -1,9 +1,10 @@
---!!class CStringList extends CList
+--!!module CCore
+--!!class CStringList extends CList @CStringList
 --!The CStringList class provides a list of strings.
---!@see CString
+--!$see CString
 CStringList = class("CStringList", CList)
 
---!!function nil CStringList(string ...)
+--!!member function nil CStringList(string ...) @CStringList()
 function CStringList:initialize(...)
     local list = {...}
     for i, v in ipairs(list) do
@@ -12,14 +13,14 @@ function CStringList:initialize(...)
     CList.initialize(self, unpack(list))
 end
 
---!!function CStringList.fromList(CList list)
---!@static
+--!!member function CStringList.fromList(CList list) @CStringList.fromList
+--!$static
 function CStringList.static.fromList(list)
     assert(instanceOf(CList, list), "List not instance of CList")
     return CStringList(list:unpack())
 end
 
---!!function string CStringList:concat(string separator, int i, int j)
+--!!member function string CStringList:concat(string separator, int i, int j) @CStringList:concat
 function CStringList:concat(sep, i, j)
     sep = sep or ""
     i = i or 1
@@ -27,7 +28,7 @@ function CStringList:concat(sep, i, j)
     return table.concat(self, sep, i, j)
 end
 
---!!function int CStringList:find(string value, bool plain)
+--!!member function int CStringList:find(string value, bool plain) @CStringList:find
 function CStringList:find(value, plain)
     if plain then return CList.find(self, value) end
     for i,v in ipairs(self) do
@@ -36,7 +37,7 @@ function CStringList:find(value, plain)
     return nil
 end
 
---!!function int, ... CStringList:findAll(string value, bool plain)
+--!!member function int, ... CStringList:findAll(string value, bool plain) @CStringList:findAll
 function CStringList:findAll(value, plain)
     if plain then return CList.findAll(self, value) end
     local result = {}
@@ -46,7 +47,7 @@ function CStringList:findAll(value, plain)
     return unpack(result)
 end
 
---!!function nil CStringList:insert(int pos, string value)
+--!!member function nil CStringList:insert(int pos, string value) @CStringList:insert
 function CStringList:insert(pos, value)
     if value == nil then
         value = pos
